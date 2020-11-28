2ec38e0d97d668654e2593a0e699ed56426caed3
Matthew Lanigan <rintaun@gmail.com>
2020-11-27 19:00:40 -0500

    test: convert to non-inline snapshots so my editor stops formatting the whitepsace out ugh

--- src/datasource/__tests__/queries.test.ts
+++ src/datasource/__tests__/queries.test.ts
@@ -20,66 +20,25 @@ describe(QueryBuilder, () => {
   describe('clause generators', () => {
     describe('where', () => {
       it('accepts a simple object', () => {
-        expect(builder.where({ id: 1 })).toMatchInlineSnapshot(`
-          Object {
-            "sql": "WHERE (\\"any_table\\".\\"id\\" = $1)",
-            "type": "SLONIK_TOKEN_SQL",
-            "values": Array [
-              1,
-            ],
-          }
-        `)
+        expect(builder.where({ id: 1 })).toMatchSnapshot()
       })
 
       it('uses AND for multiple columns in a simple object', () => {
-        expect(builder.where({ id: 1, name: 'Bob' })).toMatchInlineSnapshot(`
-          Object {
-            "sql": "WHERE (\\"any_table\\".\\"id\\" = $1 AND \\"any_table\\".\\"name\\" = $2)",
-            "type": "SLONIK_TOKEN_SQL",
-            "values": Array [
-              1,
-              "Bob",
-            ],
-          }
-        `)
+        expect(builder.where({ id: 1, name: 'Bob' })).toMatchSnapshot()
       })
 
       it('handles multiple values for a column', () => {
-        expect(builder.where({ id: [1, 2, 3] })).toMatchInlineSnapshot(`
-          Object {
-            "sql": "WHERE (\\"any_table\\".\\"id\\" = ANY($1::integer[]))",
-            "type": "SLONIK_TOKEN_SQL",
-            "values": Array [
-              Array [
-                1,
-                2,
-                3,
-              ],
-            ],
-          }
-        `)
+        expect(builder.where({ id: [1, 2, 3] })).toMatchSnapshot()
       })
 
       it('lets you pass in a raw expression', () => {
-        expect(builder.where(sql`true`)).toMatchInlineSnapshot(`
-          Object {
-            "sql": "WHERE true",
-            "type": "SLONIK_TOKEN_SQL",
-            "values": Array [],
-          }
-        `)
+        expect(builder.where(sql`true`)).toMatchSnapshot()
       })
 
       it('accepts a list of expressions', () => {
         expect(
           builder.where([sql`true`, sql`false`, sql`more raw expressions etc.`])
-        ).toMatchInlineSnapshot(`
-          Object {
-            "sql": "WHERE (true AND false AND more raw expressions etc.)",
-            "type": "SLONIK_TOKEN_SQL",
-            "values": Array [],
-          }
-        `)
+        ).toMatchSnapshot()
       })
 
       it('sanely handles output from the `and` and `or` utilities', () => {
@@ -88,78 +47,31 @@ describe(QueryBuilder, () => {
           builder.and({ id: 2, name: 'wow' }),
         ])
 
-        expect(builder.where(conditions)).toMatchInlineSnapshot(`
-          Object {
-            "sql": "WHERE ((\\"any_table\\".\\"id\\" = $1 AND \\"any_table\\".\\"name\\" = $2) OR (\\"any_table\\".\\"id\\" = $3 AND \\"any_table\\".\\"name\\" = $4))",
-            "type": "SLONIK_TOKEN_SQL",
-            "values": Array [
-              1,
-              "asdf",
-              2,
-              "wow",
-            ],
-          }
-        `)
+        expect(builder.where(conditions)).toMatchSnapshot()
       })
 
       it('produces a valid clause with no conditions', () => {
-        expect(builder.where({})).toMatchInlineSnapshot(`
-          Object {
-            "sql": "WHERE true",
-            "type": "SLONIK_TOKEN_SQL",
-            "values": Array [],
-          }
-        `)
-        expect(builder.where([])).toMatchInlineSnapshot(`
-          Object {
-            "sql": "WHERE true",
-            "type": "SLONIK_TOKEN_SQL",
-            "values": Array [],
-          }
-        `)
+        expect(builder.where({})).toMatchSnapshot()
+        expect(builder.where([])).toMatchSnapshot()
       })
     })
 
     describe('orderBy', () => {
       it('accepts a list of column names as strings', () => {
-        expect(builder.orderBy('a', 'b', 'c')).toMatchInlineSnapshot(`
-          Object {
-            "sql": "ORDER BY \\"any_table\\".\\"a\\", \\"any_table\\".\\"b\\", \\"any_table\\".\\"c\\"",
-            "type": "SLONIK_TOKEN_SQL",
-            "values": Array [],
-          }
-        `)
+        expect(builder.orderBy('a', 'b', 'c')).toMatchSnapshot()
       })
 
       it('accepts a list of column names as an array of strings', () => {
-        expect(builder.orderBy(['a', 'b', 'c'])).toMatchInlineSnapshot(`
-          Object {
-            "sql": "ORDER BY \\"any_table\\".\\"a\\", \\"any_table\\".\\"b\\", \\"any_table\\".\\"c\\"",
-            "type": "SLONIK_TOKEN_SQL",
-            "values": Array [],
-          }
-        `)
+        expect(builder.orderBy(['a', 'b', 'c'])).toMatchSnapshot()
       })
 
       it('accepts identifier tokens', () => {
         expect(builder.orderBy(sql.identifier(['column'])))
-          .toMatchInlineSnapshot(`
-          Object {
-            "sql": "ORDER BY \\"column\\"",
-            "type": "SLONIK_TOKEN_SQL",
-            "values": Array [],
-          }
-        `)
+          .toMatchSnapshot()
       })
 
       it('accepts arbitrary sql tokens', () => {
-        expect(builder.orderBy(sql`anything i want!`)).toMatchInlineSnapshot(`
-          Object {
-            "sql": "ORDER BY anything i want!",
-            "type": "SLONIK_TOKEN_SQL",
-            "values": Array [],
-          }
-        `)
+        expect(builder.orderBy(sql`anything i want!`)).toMatchSnapshot()
       })
 
       it('accepts a mix of different types', () => {
@@ -169,56 +81,26 @@ describe(QueryBuilder, () => {
             sql.identifier(['e']),
             sql`f`,
           ])
-        ).toMatchInlineSnapshot(`
-          Object {
-            "sql": "ORDER BY \\"any_table\\".\\"a\\", \\"b\\", c, \\"any_table\\".\\"d\\", \\"e\\", f",
-            "type": "SLONIK_TOKEN_SQL",
-            "values": Array [],
-          }
-        `)
+        ).toMatchSnapshot()
       })
     })
 
     describe('groupBy', () => {
       it('accepts a list of column names as strings', () => {
-        expect(builder.groupBy('a', 'b', 'c')).toMatchInlineSnapshot(`
-          Object {
-            "sql": "GROUP BY \\"any_table\\".\\"a\\", \\"any_table\\".\\"b\\", \\"any_table\\".\\"c\\"",
-            "type": "SLONIK_TOKEN_SQL",
-            "values": Array [],
-          }
-        `)
+        expect(builder.groupBy('a', 'b', 'c')).toMatchSnapshot()
       })
 
       it('accepts a list of column names as an array of strings', () => {
-        expect(builder.groupBy(['a', 'b', 'c'])).toMatchInlineSnapshot(`
-          Object {
-            "sql": "GROUP BY \\"any_table\\".\\"a\\", \\"any_table\\".\\"b\\", \\"any_table\\".\\"c\\"",
-            "type": "SLONIK_TOKEN_SQL",
-            "values": Array [],
-          }
-        `)
+        expect(builder.groupBy(['a', 'b', 'c'])).toMatchSnapshot()
       })
 
       it('accepts identifier tokens', () => {
         expect(builder.groupBy(sql.identifier(['column'])))
-          .toMatchInlineSnapshot(`
-          Object {
-            "sql": "GROUP BY \\"column\\"",
-            "type": "SLONIK_TOKEN_SQL",
-            "values": Array [],
-          }
-        `)
+          .toMatchSnapshot()
       })
 
       it('accepts arbitrary sql tokens', () => {
-        expect(builder.groupBy(sql`anything i want!`)).toMatchInlineSnapshot(`
-          Object {
-            "sql": "GROUP BY anything i want!",
-            "type": "SLONIK_TOKEN_SQL",
-            "values": Array [],
-          }
-        `)
+        expect(builder.groupBy(sql`anything i want!`)).toMatchSnapshot()
       })
 
       it('accepts a mix of different types', () => {
@@ -228,66 +110,25 @@ describe(QueryBuilder, () => {
             sql.identifier(['e']),
             sql`f`,
           ])
-        ).toMatchInlineSnapshot(`
-          Object {
-            "sql": "GROUP BY \\"any_table\\".\\"a\\", \\"b\\", c, \\"any_table\\".\\"d\\", \\"e\\", f",
-            "type": "SLONIK_TOKEN_SQL",
-            "values": Array [],
-          }
-        `)
+        ).toMatchSnapshot()
       })
     })
 
     describe('having', () => {
       it('accepts a simple object', () => {
-        expect(builder.having({ id: 1 })).toMatchInlineSnapshot(`
-          Object {
-            "sql": "HAVING (\\"any_table\\".\\"id\\" = $1)",
-            "type": "SLONIK_TOKEN_SQL",
-            "values": Array [
-              1,
-            ],
-          }
-        `)
+        expect(builder.having({ id: 1 })).toMatchSnapshot()
       })
 
       it('uses AND for multiple columns in a simple object', () => {
-        expect(builder.having({ id: 1, name: 'Bob' })).toMatchInlineSnapshot(`
-          Object {
-            "sql": "HAVING (\\"any_table\\".\\"id\\" = $1 AND \\"any_table\\".\\"name\\" = $2)",
-            "type": "SLONIK_TOKEN_SQL",
-            "values": Array [
-              1,
-              "Bob",
-            ],
-          }
-        `)
+        expect(builder.having({ id: 1, name: 'Bob' })).toMatchSnapshot()
       })
 
       it('handles multiple values for a column', () => {
-        expect(builder.having({ id: [1, 2, 3] })).toMatchInlineSnapshot(`
-          Object {
-            "sql": "HAVING (\\"any_table\\".\\"id\\" = ANY($1::integer[]))",
-            "type": "SLONIK_TOKEN_SQL",
-            "values": Array [
-              Array [
-                1,
-                2,
-                3,
-              ],
-            ],
-          }
-        `)
+        expect(builder.having({ id: [1, 2, 3] })).toMatchSnapshot()
       })
 
       it('lets you pass in a raw expression', () => {
-        expect(builder.having(sql`true`)).toMatchInlineSnapshot(`
-          Object {
-            "sql": "HAVING true",
-            "type": "SLONIK_TOKEN_SQL",
-            "values": Array [],
-          }
-        `)
+        expect(builder.having(sql`true`)).toMatchSnapshot()
       })
 
       it('accepts a list of expressions', () => {
@@ -297,13 +138,7 @@ describe(QueryBuilder, () => {
             sql`false`,
             sql`more raw expressions etc.`,
           ])
-        ).toMatchInlineSnapshot(`
-          Object {
-            "sql": "HAVING (true AND false AND more raw expressions etc.)",
-            "type": "SLONIK_TOKEN_SQL",
-            "values": Array [],
-          }
-        `)
+        ).toMatchSnapshot()
       })
 
       it('sanely handles output from the `and` and `or` utilities', () => {
@@ -312,35 +147,12 @@ describe(QueryBuilder, () => {
           builder.and({ id: 2, name: 'wow' }),
         ])
 
-        expect(builder.having(conditions)).toMatchInlineSnapshot(`
-          Object {
-            "sql": "HAVING ((\\"any_table\\".\\"id\\" = $1 AND \\"any_table\\".\\"name\\" = $2) OR (\\"any_table\\".\\"id\\" = $3 AND \\"any_table\\".\\"name\\" = $4))",
-            "type": "SLONIK_TOKEN_SQL",
-            "values": Array [
-              1,
-              "asdf",
-              2,
-              "wow",
-            ],
-          }
-        `)
+        expect(builder.having(conditions)).toMatchSnapshot()
       })
 
       it('produces a valid clause with no conditions', () => {
-        expect(builder.having({})).toMatchInlineSnapshot(`
-          Object {
-            "sql": "HAVING true",
-            "type": "SLONIK_TOKEN_SQL",
-            "values": Array [],
-          }
-        `)
-        expect(builder.having([])).toMatchInlineSnapshot(`
-          Object {
-            "sql": "HAVING true",
-            "type": "SLONIK_TOKEN_SQL",
-            "values": Array [],
-          }
-        `)
+        expect(builder.having({})).toMatchSnapshot()
+        expect(builder.having([])).toMatchSnapshot()
       })
     })
   })
@@ -348,19 +160,7 @@ describe(QueryBuilder, () => {
   describe('core query builders', () => {
     describe('select', () => {
       it('selects everything by default', () => {
-        expect(builder.select()).toMatchInlineSnapshot(`
-          Object {
-            "sql": "
-                SELECT *
-                FROM \\"any_table\\"
-                
-                
-                
-              ",
-            "type": "SLONIK_TOKEN_SQL",
-            "values": Array [],
-          }
-        `)
+        expect(builder.select()).toMatchSnapshot()
       })
     })
 
@@ -373,24 +173,7 @@ describe(QueryBuilder, () => {
       })
 
       it('can be forced to delete everything', () => {
-        expect(builder.delete(true)).toMatchInlineSnapshot(`
-          Object {
-            "sql": "
-                WITH \\"delete_rows\\" AS (
-
-                DELETE FROM \\"any_table\\"
-
-                RETURNING *
-
-                ) SELECT *
-                  FROM \\"delete_rows\\"
-
-
-              ",
-            "type": "SLONIK_TOKEN_SQL",
-            "values": Array [],
-          }
-        `)
+        expect(builder.delete(true)).toMatchSnapshot()
       })
 
       it('builds clauses correctly', () => {
@@ -401,26 +184,7 @@ describe(QueryBuilder, () => {
             orderBy: 'id',
             having: { id: 1 },
           })
-        ).toMatchInlineSnapshot(`
-          Object {
-            "sql": "
-                WITH \\"delete_rows\\" AS (
-
-                DELETE FROM \\"any_table\\"
-                WHERE (\\"any_table\\".\\"id\\" = $1)
-                RETURNING *
-
-                ) SELECT *
-                  FROM \\"delete_rows\\"
-                  GROUP BY \\"any_table\\".\\"id\\"
-                  ORDER BY \\"any_table\\".\\"id\\"
-              ",
-            "type": "SLONIK_TOKEN_SQL",
-            "values": Array [
-              1,
-            ],
-          }
-        `)
+        ).toMatchSnapshot()
       })
     })
   })
