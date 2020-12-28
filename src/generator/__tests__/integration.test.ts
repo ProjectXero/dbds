63134e5b9c49c135ec422b15669a3f8380bc0b2e
Matthew Lanigan <rintaun@gmail.com>
2020-12-28 13:22:03 -0500

    test: integration test for Generator

---
+++ src/generator/__tests__/integration.test.ts
@@ -0,0 +1,70 @@
+import assert from 'assert'
+import { createPool, DatabasePoolType, sql } from 'slonik'
+import { raw } from 'slonik-sql-tag-raw'
+import { Generator } from '..'
+import { SchemaInfo } from '../database'
+
+let pool: DatabasePoolType
+let instance: Generator
+
+const SCHEMA = 'generator'
+
+beforeAll(async () => {
+  assert(process.env.DATABASE_URL, 'DATABASE_URL must be configured')
+
+  pool = createPool(process.env.DATABASE_URL, {
+    captureStackTrace: true,
+    maximumPoolSize: 1,
+    idleTimeout: 'DISABLE_TIMEOUT',
+    idleInTransactionSessionTimeout: 'DISABLE_TIMEOUT',
+  })
+
+  await pool.query(sql`CREATE SCHEMA ${sql.identifier([SCHEMA])}`)
+  await pool.query(sql`SET search_path TO ${raw(SCHEMA)}`)
+
+  await pool.query(sql`
+    CREATE TYPE "test_type_enum" AS ENUM (
+      'aa',
+      'bb',
+      'zzzz',
+      'cc'
+    )
+  `)
+  await pool.query(sql`
+    CREATE DOMAIN "test_type_domain" AS text
+  `)
+  await pool.query(sql`
+    CREATE TABLE "test_table_standard" (
+      id SERIAL NOT NULL,
+      arr_test TEXT[],
+      domain_test TEST_TYPE_DOMAIN,
+      enum_test TEST_TYPE_ENUM NOT NULL,
+      enum_arr_test TEST_TYPE_ENUM[],
+      jsonb_test JSONB
+    )
+  `)
+  await pool.query(sql`
+    CREATE TABLE "test_table_types" (
+      id UUID NOT NULL,
+      table_test TEST_TABLE_STANDARD,
+      table_arr_test TEST_TABLE_STANDARD[] DEFAULT '{}' NOT NULL
+    )
+  `)
+  await pool.query(sql`
+    CREATE TABLE "test_table_order" (
+      subsequent_table_type TEST_TABLE_STANDARD
+    )
+  `)
+})
+
+beforeEach(() => {
+  instance = new Generator({
+    schema: new SchemaInfo(pool, SCHEMA),
+  })
+})
+
+describe('Generator', () => {
+  it('generates the correct types', async () => {
+    expect(await instance.build()).toMatchSnapshot()
+  })
+})
