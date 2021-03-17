ffcad1e7943a63be157625aec366bb96332613e7
Matthew Lanigan <rintaun@gmail.com>
2021-03-17 16:56:25 -0400

    chore: remove obsolete slonik-sql-tag-raw types
    it's in typescript now yay

--- src/types/slonik-sql-tag-raw/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'slonik-sql-tag-raw' {
-  import type { ValueExpressionType, SqlSqlTokenType } from 'slonik'
-
-  export function raw(
-    sql: string,
-    values?: ReadonlyArray<ValueExpressionType>
-  ): SqlSqlTokenType
-}
