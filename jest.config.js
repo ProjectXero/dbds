5ec10c731fca953ccb798a138fbc62bfef069faa
Matthew Lanigan <rintaun@gmail.com>
2022-04-14 22:16:26 -0400

    ci: generate and upload coverage reports

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,16 @@
+/** @type {import('ts-jest').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: ['/node_modules/', '/__fixtures__/', '/dist/'],
   globalSetup: '<rootDir>/jest.setup.ts',
   globalTeardown: '<rootDir>/jest.teardown.ts',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/testing/**',
+    '!**/__tests__/**',
+    "!**/node_modules/**",
+  ],
+  
 }
